53f2946061a39d51b0f35346fbaeb636f576b98f
Bibhu Prasad Saha <69378021+Bibs24@users.noreply.github.com>
2022-06-08 10:54:03 +0530

    Time: 5 ms (43.27%), Space: 44.1 MB (40.01%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
@@ -1,14 +1,17 @@
 class Solution {
-public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        int arr[1001] = {0};
-        for(auto x: nums1){
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int arr[] = new int[1001];
+        for(int x: arr){
+            arr[x] = 0;
+        }
+        for(int x: nums1){
             arr[x]++;
         }
-        vector<int> ans;
-        for(auto x: nums2){
-            if(arr[x] !=0) ans.push_back(x),arr[x]=0;
+        List<Integer> li = new ArrayList<>();
+        for(int x: nums2){
+            if(arr[x] != 0) li.add(x); arr[x] = 0;
         }
+        int[] ans = li.stream().mapToInt(Integer::intValue).toArray();
         return ans;
     }
-};
+}
