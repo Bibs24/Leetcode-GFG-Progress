95fd1771e8ce51bc26a28b1fa73997c136878386
Bibhu Prasad Saha <69378021+Bibs24@users.noreply.github.com>
2022-06-26 11:08:39 +0530

    Added solution - LeetHub

--- Rotate Array - GFG/rotate-array.cpp
+++ Rotate Array - GFG/rotate-array.cpp
@@ -10,18 +10,23 @@ class Solution{
     //Function to rotate an array by d elements in counter-clockwise direction. 
     void rotateArr(int arr[], int d, int n){
         // code here
-        if(d<n){
-            reverse(arr+d,arr+n);
-            reverse(arr,arr+d);
-            reverse(arr,arr+n);
-        }else{
-            if(d>n){
-                d = d-n;
-                reverse(arr+d,arr+n);
-                reverse(arr,arr+d);
-                reverse(arr,arr+n);
-            }
-        }
+        // if(d<n){
+        //     reverse(arr+d,arr+n);
+        //     reverse(arr,arr+d);
+        //     reverse(arr,arr+n);
+        // }else{
+        //     if(d>n){
+        //         d = d-n;
+        //         reverse(arr+d,arr+n);
+        //         reverse(arr,arr+d);
+        //         reverse(arr,arr+n);
+        //     }
+        // }
+        d = d%n;
+        reverse(arr,arr+d);
+        reverse(arr+d,arr+n);
+        reverse(arr,arr+n);
+        
     }
 };
 
