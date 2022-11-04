817ecddd2f578acc40d67e49acba2000974916cb
Bibhu Prasad Saha <69378021+Bibs24@users.noreply.github.com>
2022-11-04 12:05:25 +0530

    Time: 16 ms (49.29%), Space: 8 MB (27.79%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -15,13 +15,6 @@ public:
         }
         string res;
         helper(temp,res);
-        // for(int i=0;i<s.size()-1;i++){
-        //     if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' ||s[i] == 'U'){
-        //         fin+=res[i];
-        //     }else{
-        //         fin+=s[i];
-        //     }
-        // }
         int j=0;
         for(auto &i:s){
              if(i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u' || i == 'A' || i == 'E' || i == 'I' || i == 'O' ||i == 'U'){
