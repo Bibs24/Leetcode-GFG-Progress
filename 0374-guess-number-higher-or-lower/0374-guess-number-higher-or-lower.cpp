1120997a6d963c22dd4b18106a7be1f2e6dcdb44
Bibhu Prasad Saha <69378021+Bibs24@users.noreply.github.com>
2022-11-16 22:56:43 +0530

    Time: 5 ms (14.09%), Space: 6 MB (22.94%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -11,13 +11,26 @@ class Solution {
 public:
     int guessNumber(int n, long s = 1) {
         
-        while(s <= n){
-            int x = (s+n)/2;
-            int g = guess(x);
-            if(g == 0) return x;
-            if(g < 0) n = x-1;
-            if(g > 0) s = x+1;
+        // while(s <= n){
+        //     int x = (s+n)/2;
+        //     int g = guess(x);
+        //     if(g == 0) return x;
+        //     if(g < 0) n = x-1;
+        //     if(g > 0) s = x+1;
+        // }
+        // return -1;
+        int l = 1, r = n;
+        
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(guess(mid) == 0){
+                return mid;
+            }else if (guess(mid) == -1){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
         }
-        return -1;
+        return 0;
     }
 };
